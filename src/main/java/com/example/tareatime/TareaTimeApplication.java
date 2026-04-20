41e8303ffc9c31a1158604633734a357cfc3ba47
Alancito77 <alanhernandez1918@gmail.com>
2026-04-20 00:02:24 -0600

    Proyecto en 0 Spring boot

---
+++ src/main/java/com/example/tareatime/TareaTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tareatime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tareatime/TareaTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tareatime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
